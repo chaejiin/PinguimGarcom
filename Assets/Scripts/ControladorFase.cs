b3efafe2784a0b0a60166e4f8b336ae905f90cc4
LEANDRO CHAEJIIN CASSIMIRO ASSANO <leandro.ccassano@senacsp.edu.br>
2025-02-25 17:33:34 -0300

    25/02

---
+++ Assets/Scripts/ControladorFase.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ControladorFase : MonoBehaviour
+{
+
+    internal float TempoRestante;
+
+    public GameObject telaGanhou, telaPerdeuErrou, telaTempoEsgotado;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
